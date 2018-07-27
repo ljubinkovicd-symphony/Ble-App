ab7ddb28d257e3d819bcc8679c52d62fd3150c88
Djordje Ljubinkovic <djordje.ljubinkovic@symphony.is>
2018-07-27 17:21:38 +0200

    Add react-native-navigation library to support navigation between screens inside the app.

--- index.js
+++ index.js
@@ -3,5 +3,9 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import { Navigation } from 'react-native-navigation';
+import { registerScreens } from './screens';
+
+registerScreens();
 
 AppRegistry.registerComponent(appName, () => App);
